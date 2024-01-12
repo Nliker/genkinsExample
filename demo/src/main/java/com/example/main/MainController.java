a6c716980e8e769ce9d4ef4602e9643ee98a42fa
tjwjdgus83 <tjwjdgus83@naver.com>
2024-01-12 12:23:06 +0900

    initial commit

---
+++ demo/src/main/java/com/example/main/MainController.java
@@ -0,0 +1,26 @@
+package com.example.main;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MainController {
+  @GetMapping("/ping")
+  public ResponseEntity<Map<String,String>> ping(){
+    Map<String,String> result=new HashMap<>();
+    result.put("result","pong");
+    return ResponseEntity.ok().body(result);
+  }
+
+  @GetMapping("/test")
+  public ResponseEntity<Map<String,String>> test(){
+    Map<String,String> result=new HashMap<>();
+    result.put("result","test");
+    return ResponseEntity.ok().body(result);
+  }
+}
